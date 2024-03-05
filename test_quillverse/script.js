bf1a16bea0bc4729cecb29ebced993c566fc205a
Quillverse <158827661+Quillverse@users.noreply.github.com>
2024-03-05 06:15:49 +0530

    Add files via upload

--- test_quillverse/script.js
+++ test_quillverse/script.js
@@ -2,6 +2,16 @@ const container = document.getElementById('log-in-container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled')
+    }
+})
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
@@ -9,3 +19,8 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+function myFun() {
+    var divs = document.getElementById('dropdown');
+    divs.classList.toggle('show');
+}
