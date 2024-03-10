8c2130dac4946ef0ad3e9448b8f61752d188ecc1
Quillverse <158827661+Quillverse@users.noreply.github.com>
2024-03-10 22:06:47 +0530

    Add files via upload

--- test_quillverse/script.js
+++ test_quillverse/script.js
@@ -1,18 +1,3 @@
-const container = document.getElementById('log-in-container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-const carousel = document.querySelector(".book-wrapper");
-const arrowBtn = document.querySelectorAll(".book-wrapper i");
-const firstCardWidth = carousel.querySelector(".book-content").offsetWidth;
-
-// slider
-
-arrowBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
-    })
-});
-
 
 // navbar scroll change
 
@@ -29,6 +14,10 @@ document.addEventListener('scroll', () => {
 
 // sign-up toggle
 
+const container = document.getElementById('log-in-container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
@@ -37,49 +26,16 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-// dropdown
-
-function myFun() {
-    var divs = document.getElementById('dropdown');
-    divs.classList.toggle('show');
-}
 
 // slider
 
-var slideIndex = 1;
-showSlides(slideIndex);
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+const carousel = document.querySelector(".book-wrapper");
+const arrowBtn = document.querySelectorAll(".book-wrapper i");
+const firstCardWidth = carousel.querySelector(".book-content").offsetWidth;
 
-var slideIndex = 0;
-showSlides();
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
+
+arrowBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
