ce9a60af57f241a5e9e993e853c32cb8ab9c4bf7
Quillverse <158827661+Quillverse@users.noreply.github.com>
2024-03-13 22:16:47 +0530

    Add files via upload

---
+++ test_quillverse/newLanding.js
@@ -0,0 +1,11 @@
+const heroBtn = document.querySelector('.hero-btn');
+const navLoginBtn = document.querySelector('.login-btn');
+
+heroBtn.addEventListener('click', () => {
+    window.location.href = "file:///C:/Users/usera/OneDrive/Desktop/clone2/Quillverse/test_quillverse/login.html";
+})
+
+navLoginBtn.addEventListener('click', () => {
+    window.location.href = "file:///C:/Users/usera/OneDrive/Desktop/clone2/Quillverse/test_quillverse/login.html";
+})
+
