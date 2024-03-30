ae7e454fb60bb6e49679225ed4fdb5c18b3d7797
Quillverse <158827661+Quillverse@users.noreply.github.com>
2024-03-30 18:11:26 +0530

    Update newLanding.js

--- test_quillverse/newLanding.js
+++ test_quillverse/newLanding.js
@@ -1,11 +1,29 @@
-const heroBtn = document.querySelector('.hero-btn');
-const navLoginBtn = document.querySelector('.login-btn');
+document.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
 
-heroBtn.addEventListener('click', () => {
-    window.location.href = "file:///C:/Users/usera/OneDrive/Desktop/clone2/Quillverse/test_quillverse/login.html";
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled')
+    }
 })
 
-navLoginBtn.addEventListener('click', () => {
-    window.location.href = "file:///C:/Users/usera/OneDrive/Desktop/clone2/Quillverse/test_quillverse/login.html";
-})
+const scrollLinks = document.querySelectorAll('.scroll-link');
+const navBar = document.querySelector('.navbar');
 
+scrollLinks.forEach(function (link) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id);
+
+        const navHeight = navBar.getBoundingClientRect().height;
+        let position = element.offsetTop - navHeight;
+
+        window.scrollTo({
+            top: position,
+            left: 0,
+        });
+    });
+});
